b49654bb0298d61d420f22baf8e7dda1d305d839
BonnW <pwonghansa@gmail.com>
2017-12-15 15:54:37 -0600

    packages installed, Error with bcrypt, userModels Schema created.

--- api/models/userModels.js
+++ api/models/userModels.js
@@ -3,6 +3,24 @@ const Schema = mongoose.Schema;
 
 const UserSchema = Schema({
   // create your user schema here.
+  firstName: {
+    type: String,
+    required: true
+  },
+  userName: {
+    type: String,
+    required: true,
+    unique: true
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+  }
 });
 
 module.exports = mongoose.model('User', UserSchema);
